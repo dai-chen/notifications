163a813edbd2df67f4cf08a1204f1be7a4ea6bea
Chen Dai <daichen@amazon.com>
2021-07-22 12:32:43 -0700

    Add SNS client and UT
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- notifications/spi/src/test/kotlin/org/opensearch/notifications/spi/client/DestinationSNSClientTests.kt
+++ notifications/spi/src/test/kotlin/org/opensearch/notifications/spi/client/DestinationSNSClientTests.kt
@@ -10,6 +10,8 @@
  */
 package org.opensearch.notifications.spi.client
 
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.services.sns.AmazonSNS
 import com.amazonaws.services.sns.AmazonSNSClientBuilder
@@ -18,30 +20,33 @@ import com.nhaarman.mockitokotlin2.whenever
 import io.mockk.every
 import io.mockk.mockkStatic
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.Mockito.any
-import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.opensearch.common.settings.SecureString
 import org.opensearch.notifications.spi.model.MessageContent
 import org.opensearch.notifications.spi.model.destination.SNSDestination
 import kotlin.test.assertEquals
 
+@RunWith(MockitoJUnitRunner::class)
 internal class DestinationSNSClientTests {
 
+    @Mock
+    private lateinit var amazonSNS: AmazonSNS
+
+    @Mock
+    private lateinit var builder: AmazonSNSClientBuilder
+
     @Test
-    fun `SNS client should use default credential to publish message`() {
+    fun `SNS client should use default credential to publish message if no IAM credential`() {
         val region = "us-east-1"
         val title = "Test"
         val body = "This is a test"
         val topicArn = "arn:aws:sns:${region}:012345678989:test-notification"
         val messageId = "message-1"
 
-        val amazonSNS = mock(AmazonSNS::class.java)
-        val builder = mock(AmazonSNSClientBuilder::class.java)
-        mockkStatic(AmazonSNSClientBuilder::standard)
-        every { AmazonSNSClientBuilder.standard() } returns builder
-        whenever(builder.withRegion(region)).thenReturn(builder)
-        whenever(builder.withCredentials(any(DefaultAWSCredentialsProviderChain::class.java)))
-            .thenReturn(builder)
-        whenever(builder.build()).thenReturn(amazonSNS)
+        mockBuilderChain(region, DefaultAWSCredentialsProviderChain::class.java)
         whenever(amazonSNS.publish(topicArn, body, title))
             .thenReturn(PublishResult().withMessageId(messageId))
 
@@ -53,7 +58,31 @@ internal class DestinationSNSClientTests {
     }
 
     @Test
-    fun `SNS client should use IAM credential to publish message`() {
+    fun `SNS client should use IAM credential to publish message if configured`() {
+        val region = "us-west-2"
+        val title = "Hello"
+        val body = "World"
+        val topicArn = "arn:aws:sns:${region}:012345678989:test-notification"
+        val accessKey = SecureString("access-key".toCharArray())
+        val secretKey = SecureString("secret-key".toCharArray())
+        val messageId = "message-2"
 
+        mockBuilderChain(region, AWSStaticCredentialsProvider::class.java)
+        whenever(amazonSNS.publish(topicArn, body, title))
+            .thenReturn(PublishResult().withMessageId(messageId))
+
+        val message = MessageContent(title, body)
+        val destination = SNSDestination(topicArn, null, accessKey, secretKey)
+        val snsClient = DestinationSNSClient(destination)
+        val result = snsClient.execute(topicArn, message)
+        assertEquals(messageId, result)
+    }
+
+    private fun <T: AWSCredentialsProvider> mockBuilderChain(region: String, providerType: Class<T>) {
+        mockkStatic(AmazonSNSClientBuilder::standard)
+        every { AmazonSNSClientBuilder.standard() } returns builder
+        whenever(builder.withRegion(region)).thenReturn(builder)
+        whenever(builder.withCredentials(any(providerType))).thenReturn(builder)
+        whenever(builder.build()).thenReturn(amazonSNS)
     }
 }
